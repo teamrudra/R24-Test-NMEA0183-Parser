b818d964ba971824fe4d3f36839c8b242701c3f7
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-08-28 15:00:28 +0530

    gps: Add test for parsing GLL

--- tests/test_parsing.c
+++ tests/test_parsing.c
@@ -5,7 +5,8 @@
 
 void test_gga_parsing_over_examples(void) {
   const char* examples[] = {
-    "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,,0000*5E"};
+    "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,,0000*5E",
+  };
   gps_t handle = gps_init();
   TEST_ASSERT_EQUAL(GPS_NO_ERROR, gps_update(handle, examples[0], strlen(examples[0])));
   int latlon_degmins[2];
@@ -17,11 +18,20 @@ void test_gga_parsing_over_examples(void) {
   TEST_ASSERT_EQUAL(3858, latlon_minfracs[1]);
 }
 
+void test_gll_parsing_over_examples(void) {
+  const char* examples[] = {
+    "$GPGLL,3723.2475,N,12158.3416,W,161229.487,A,A*41",
+  };
+  gps_t handle = gps_init();
+  TEST_ASSERT_EQUAL(GPS_NO_ERROR, gps_update(handle, examples[0], strlen(examples[0])));
+}
+
 void setUp() {}
 void tearDown() {}
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_gga_parsing_over_examples);
+  RUN_TEST(test_gll_parsing_over_examples);
   return UNITY_END();
 }
